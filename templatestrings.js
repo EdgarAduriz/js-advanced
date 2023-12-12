47e61d09756f2f1f1c319bd4767652ef4bebcbbe
Riccardo Grassi <riccardograssi66@gmail.com>
2023-12-12 16:32:59 +0100

    Caricati esercizi Template strings

---
+++ templatestrings.js
@@ -0,0 +1,16 @@
+//Esercizio 1
+const num1 = 10;
+const num2 = 20;
+const sum = `La somma dei numeri è ${num1 + num2}`;
+console.log(sum)
+//Esercizio 2
+let esercizio2 = `Utilizza una template string
+ per creare una stringa
+  su più righe che includa
+   interruzioni di riga.`;
+console.log(esercizio2)
+//Esercizio 3
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = `Hello ${name}! ${message}`;
+console.log(greeting)
